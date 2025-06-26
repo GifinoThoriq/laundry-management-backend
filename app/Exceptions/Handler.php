e395e47495cf12d74aea8bf7f0a878979200fec3
Gifino Thoriq <gifinothoriq@Gifinos-MacBook-Air.local>
2025-06-26 15:26:25 +0700

    fix(Handler): auth exception issue

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\AuthenticationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -12,6 +13,7 @@ class Handler extends ExceptionHandler
     {
         return response()->json([
             'meta' => [
+
                 'status' => 422,
                 'message' => 'Validation error',
             ],
@@ -21,7 +23,16 @@ class Handler extends ExceptionHandler
 
     // Optional: still include report() and render() methods
     public function register(): void
-    {
-        //
+    {   
+
+        //unauthorized method
+        $this->renderable(function (AuthenticationException $e, $request) {
+            return response()->json([
+                'meta' => [
+                    'status' => 401,
+                    'message' => 'Unauthorized or token invalid.',
+                ],
+            ], 401);
+        });
     }
 }
